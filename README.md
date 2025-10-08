fc82bee6aaf01a0a46759bc8cc74dfe9c38b0060
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2025-10-08 19:00:05 +0300

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# sergiye.StatusPageIoApi
+
+[![Nuget](https://img.shields.io/nuget/v/StatusPageIoApi?style=for-the-badge)](https://www.nuget.org/packages/StatusPageIoApi/)
+[![Nuget](https://img.shields.io/nuget/dt/StatusPageIoApi?label=nuget-downloads&style=for-the-badge)](https://www.nuget.org/packages/StatusPageIoApi/)
+
+*StatusPageIoApi is an implementation of StatusPage.io's API in C# / .NET*
+
+## Developer information
+
+
+> [!CAUTION]
+> This package is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
---
+++ make.bat
@@ -0,0 +1,19 @@
+@echo off
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) DO (
+SET msbuild="%%F"
+)
+ECHO %msbuild%
+
+@%msbuild% StatusPageIoApi.sln /t:restore /p:RestorePackagesConfig=true
+@%msbuild% StatusPageIoApi.sln /t:Rebuild /p:DebugType=None /p:Configuration=Release
+
+rem dotnet build StatusPageIoApi.sln -c Release -p:DebugType=None -p:Platform="Any CPU"
+rem dotnet pack StatusPageIoApi.sln -c Release -p:DebugType=None -p:Platform="Any CPU"
+
+if errorlevel 1 goto error
+
+goto exit
+:error
+pause
+:exit
